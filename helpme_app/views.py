d90c81d8f256681b1454de2b50b3e2d68d4617f1
Pheonnexx <pheonn@hotmail.co.uk>
2017-01-11 20:21:19 +0000

    getting it to send texts

--- helpme_app/views.py
+++ helpme_app/views.py
@@ -7,7 +7,7 @@ from helpme_app import ask, app
 from flask import render_template
 from flask_ask import Ask, statement, question, session
 
-client = messagebird.Client('uSdG4buXOJDltanV22eCNhXbY')
+client = messagebird.Client('cUTQcjhDaqfscs0pKHhGQk6aR')
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
 
@@ -21,14 +21,30 @@ def start_helpme_app():
 @ask.intent("HelpMeFriendIntent", convert={'helpmefriend': str})
 def get_help_from_friend(helpmefriend):
     #message person that has been selected from contact list
-    message = client.message_create(
+    msg = client.message_create(
         'HEATHER',
-        '07960207329',
+        '+447960207329',
         'This is a test message.',
         { 'reference' : 'Heather' }
     )
-
-    if message.success:
+    import pdb; pdb.set_trace()
+    print('\nThe following information was returned as a Message object:\n')
+    print('  id                : %s' % msg.id)
+    print('  href              : %s' % msg.href)
+    print('  direction         : %s' % msg.direction)
+    print('  type              : %s' % msg.type)
+    print('  originator        : %s' % msg.originator)
+    print('  body              : %s' % msg.body)
+    print('  reference         : %s' % msg.reference)
+    print('  validity          : %s' % msg.validity)
+    print('  gateway           : %s' % msg.gateway)
+    print('  typeDetails       : %s' % msg.typeDetails)
+    print('  datacoding        : %s' % msg.datacoding)
+    print('  mclass            : %s' % msg.mclass)
+    print('  scheduledDatetime : %s' % msg.scheduledDatetime)
+    print('  createdDatetime   : %s' % msg.createdDatetime)
+    print('  recipients        : %s\n' % msg.recipients.items.recipient)
+    if msg:
         msg = render_template('get_help_from_friend', helpmefriend = helpmefriend)
     else:
         msg = render_template('unable_to_contact_person', helpmefriend = helpmefriend)
